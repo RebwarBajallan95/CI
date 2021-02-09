929a293e6c25c6a07986d3abb26e72bb173b8482
Rebwar Ali Omer Bajallan <rebwar@Rebwars-MacBook-Pro.local>
2021-02-09 09:13:28 +0100

    FEAT: Clone repo - closes #3

--- src/buildtools/CloneRepo.java
+++ src/buildtools/CloneRepo.java
@@ -1,6 +1,8 @@
 package buildtools;
 
 import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,9 +14,26 @@ public class CloneRepo {
     private File tempDirectory;
 
 
-    CloneRepo(String tempDirPath) throws IOException {
+    CloneRepo(String gitUrl, String branchName, String tempDirPath) throws IOException, GitAPIException {
         tempDirectory = createTempDir(tempDirPath);
         System.out.println(tempDirectory + " created!");
+        cloneRepository(gitUrl, branchName, tempDirectory);
+    }
+
+
+    /*
+       Clone git repository
+    */
+    private void cloneRepository(String gitUrl, String branchName, File tempDir) throws GitAPIException {
+        System.out.println("Cloning from " + gitUrl + " to " + tempDir);
+        try (Git result = Git.cloneRepository()
+                .setURI(gitUrl)
+                .setBranch(branchName)
+                .setDirectory(tempDir)
+                .call()) {
+            // Note: the call() returns an opened repository already which needs to be closed to avoid file handle leaks!
+            System.out.println("Having repository: " + result.getRepository().getDirectory());
+        }
     }
 
     /*
@@ -51,8 +70,11 @@ public class CloneRepo {
     public File getTempDir(){ return tempDirectory; }
 
 
-    public static void main(String[] args) throws IOException {
-        CloneRepo cloneTest = new CloneRepo(".");
+    public static void main(String[] args) throws IOException, GitAPIException {
+        CloneRepo cloneTest = new CloneRepo("https://github.com/RebwarBajallan95/RandomProject",
+                                        "testBranch",
+                                        "./temp"
+                                );
 
     }
 
