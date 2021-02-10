fd7f4fff67eaed61f89c6ece3e5abb4ba4a1b33c
Vidarr <luckri@hotmail.se>
2021-02-10 18:10:31 +0100

    FEAT/BUG: Refactored database related code into own class, and fixed nullpointerexception - closes #22, closes #23

--- src/logserv/src/java/servlet/BuildServlet.java
+++ src/logserv/src/java/servlet/BuildServlet.java
@@ -17,13 +17,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import servlet.DBUtils.*;
 
 /**
  *
- * @author Simon
+ * @author Simon, Lucas
  */
 public class BuildServlet extends HttpServlet {
-    
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -31,78 +31,15 @@ public class BuildServlet extends HttpServlet {
 
         ArrayList<Build> builds = new ArrayList<Build>();
         String queryId = request.getParameter("id");
-
-        String sqlFetchWithId = "SELECT id, identifier, status, buildlog, timecreated FROM builds WHERE id = ?;";
-        String sqlFetchNoId = "SELECT id, identifier, status, buildlog, timecreated FROM builds";
+        DBUtils dbu = new DBUtils();
 
         if(queryId !=null) {
-            try (Connection conn = connect();
-                 PreparedStatement pstmt = conn.prepareStatement(sqlFetchWithId)){
-
-                 pstmt.setInt(1, Integer.parseInt(queryId));
-                 ResultSet res = pstmt.executeQuery();
-
-                 while(res.next()){
-                     Build tempBuild = new Build();
-                     tempBuild.setIdentifier(res.getString("identifier"));
-                     tempBuild.setStatus(res.getString("status"));
-                     tempBuild.setBuildlog(res.getString("buildlog"));
-                     tempBuild.setTimecreated(res.getTime("timecreated"));
-                     builds.add(tempBuild);
-                 }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
+            builds = dbu.fetchWithId(Integer.parseInt(queryId));
         } else {
-            try (Connection conn = connect();
-                 Statement stmt = conn.createStatement()){
-
-                ResultSet res = stmt.executeQuery(sqlFetchNoId);
-
-                while(res.next()){
-                    Build tempBuild = new Build();
-                    tempBuild.setIdentifier(res.getString("identifier"));
-                    tempBuild.setStatus(res.getString("status"));
-                    tempBuild.setBuildlog(res.getString("buildlog"));
-                    tempBuild.setTimecreated(res.getTime("timecreated"));
-                    builds.add(tempBuild);
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            builds = dbu.fetchWithNoId();
         }
-        
+
         request.setAttribute("buildList", builds);
         getServletConfig().getServletContext().getRequestDispatcher("/builds.jsp").forward(request,response);
-        
-        }
-
-    private Connection connect(){
-        String url = "jdbc:sqlite:./db/logs.db";
-        String tableCreationString = "CREATE TABLE IF NOT EXISTS build (\n"
-                + "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
-                + "identifier TEXT,\n"
-                + "    status TEXT,\n"
-                + "    buildlog TEXT,\n"
-                + "    timecreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP\n"
-                + ");\n";
-
-        Connection retConn = null;
-
-        try (Connection conn = DriverManager.getConnection(url)){
-            if (conn != null){
-                DatabaseMetaData meta = conn.getMetaData();
-                Statement stmt = conn.createStatement();
-                stmt.execute(tableCreationString);
-                retConn = conn;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return retConn;
     }
 }
