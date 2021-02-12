f0d5b2c47f3fa5d0fdb93d7b7e3e189882154d7a
Johan <johanbes@kth.se>
2021-02-12 18:00:19 +0100

    feat: assessment branch for target project done - closes #31

---
+++ src/isPrime.java
@@ -0,0 +1,24 @@
+/**
+ * Class to determine if an integer
+ * is a prime number or not
+ */
+public class isPrime {
+    /**
+     * Funktion to determine if an Integer
+     * is a prime number or not
+     * @param n the number to be tested
+     * @return boolean value whether value
+     * is prime or not
+     */
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/isPrimeTest.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+    /**
+     * Initiate arguments to the tests
+     */
+    public class isPrimeTest {
+        int INTEGER_BELOW_TWO = 1;
+        int INTEGER_PRIME = 3;
+        int INTEGER_EVEN_NUMBER = 12;
+        int INTEGER_ODD_NOT_PRIME = 51;
+    /**
+     * Initiate all tests
+     */
+    public boolean[] runTests(){
+        return new boolean[]{test_One(),test_Two(),test_Three(),test_Four()};
+    }
+
+    /**
+     * Test with an integer below two.
+     * Expected to return false
+     * @return if the tests failed or not
+     * when using an argument lesser than two
+     */
+    private boolean test_One(){
+        return !isPrime.isPrime(INTEGER_BELOW_TWO) ? true : false;
+    }
+
+    /**
+     * Test with a prime number. Expected
+     * to return true since three is
+     * a prime number
+     * @return if the tests fails or not
+     */
+    private boolean test_Two(){
+        return isPrime.isPrime(INTEGER_PRIME) ? true : false;
+    }
+
+    /**
+     * Test with an even integer above two
+     * Should return to false
+     * @return if the tests fails or not
+     */
+    private boolean test_Three(){
+        return !isPrime.isPrime(INTEGER_EVEN_NUMBER) ? true : false;
+    }
+
+    /**
+     * Test with an odd non-prime integer
+     * should return false
+     * @return if the test fails or not
+     */
+    private boolean test_Four(){
+        return !isPrime.isPrime(INTEGER_ODD_NOT_PRIME) ? true : false;
+    }
+
+    /**
+     * method to initiate the tests of
+     * isPrime
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        isPrimeTest primeTest = new isPrimeTest();
+        boolean[] testResults = primeTest.runTests();
+        int nrOfTests = testResults.length;
+        int nrOfSuccededTests = 0;
+        for (int i = 0;i < nrOfTests;i++){
+            if (testResults[i]){
+            nrOfSuccededTests = nrOfSuccededTests + 1; }
+        }
+        try{
+                File resultsFile = new File("results.txt");
+                resultsFile.setWritable(true);
+                FileWriter fw = new FileWriter("results.txt");
+                fw.write("------ TEST RESULTS ------ \n");
+                fw.write((nrOfTests == nrOfSuccededTests ? "SUCCESS\n" : "FAILED\n"));
+                fw.write(nrOfSuccededTests + " OUT OF " + nrOfTests + " TESTS PASSED\n");
+                fw.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
