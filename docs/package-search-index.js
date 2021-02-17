68baeb50f87abdb0901f4e0fc612786fd3a49b12
Johan <johanbes@kth.se>
2021-02-17 20:27:28 +0100

    feat: add javadoc - closes #44

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"bean"},{"l":"buildtools"},{"l":"servlet"},{"l":"testTools"}]
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"bean","l":"Build"},{"p":"servlet","l":"BuildServlet"},{"p":"buildtools","l":"CIServer"},{"p":"buildtools","l":"CloneRepo"},{"p":"buildtools","l":"CompileFiles"},{"p":"servlet","l":"DBUtils"},{"p":"buildtools","l":"SendMail"},{"p":"testTools","l":"Tests"},{"p":"buildtools","l":"WebhookParser"}]
